e2d7cd20360ede1e65b56dc5785e05332d36d3a1
waseem ali <60802736+aliw2025@users.noreply.github.com>
2022-06-03 16:30:23 +0500

    changes

--- resources/views/patient-360/patients.blade.php
+++ resources/views/patient-360/patients.blade.php
@@ -47,7 +47,7 @@
       <div class="table-responsive">
         <table class="table table-hover">
           <thead>
-            <tr>
+          <tr>
               <th>Project</th>
               <th>Client</th>
               <th>Users</th>
